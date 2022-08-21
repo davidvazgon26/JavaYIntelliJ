296d72bfc96bde741f4e64b458669f76e04f9e1d
davidvazgon26 <davidvazgon@hotmail.com>
2022-08-21 00:03:02 -0500

    mas ejercicios de java

--- src/com/example/java/tecese/Polymorphism.java
+++ src/com/example/java/tecese/Polymorphism.java
@@ -1,7 +1,10 @@
 package com.example.java.tecese;
 
 import java.io.*;
+import java.util.HashSet;
 
+//Una clase principal que tiene un metodo filtes que devuelve nulo (no hace nada el metodo)
+//Declarar 2 variables de tipo entero y publicas en la clase padre llamadas startElement y endElement
 class Parent{
     public int startElement;
     public int endElement;
@@ -9,6 +12,8 @@ class Parent{
         return null;
     }
 }
+
+//Clase que se extiende de Parent y anula el metodo filter o lo sobre escribe para escribir su propia version
 class ChildOne extends Parent {
     private Boolean isPrime(int n){
         if(n==1) return false;
@@ -16,7 +21,7 @@ class ChildOne extends Parent {
             if(n%i == 0) return false;
         return true;
     }
-    @Override
+    @Override //Sobre escribo el metodo para devolver los numeros primos
     public String filter(){
         StringBuilder sb = new StringBuilder();
         for(int i=startElement;i<=endElement;i++)
@@ -26,29 +31,52 @@ class ChildOne extends Parent {
 }
 
 
+//Clase que se extiende de Parent y anula el metodo filter o lo sobre escribe para escribir su propia version
 class ChildTwo extends Parent{
-    private int numSquareSum(int n){
-        int sum = 0;
-        while(n!=0){
-            int temp = n % 10;
-            n /= 10;
-            sum += temp*temp;
+    private int sumDigitSquare(int n){
+
+        int sq = 0;
+        while (n > 0)
+        {
+            int digit = n % 10;
+            sq += digit * digit;
+            n = n / 10;
         }
-        return sum;
+        return sq;
     }
 
-    private Boolean isHappy(int n){
-        int fast = n;
-        int slow = n;
-        do{
-            slow = numSquareSum(slow);
-            fast = numSquareSum(numSquareSum(fast));
-        }
-        while(slow!=fast);
+    private Boolean isHappy(int n) {
+        // A set to store numbers during
+        // repeated square sum process
+        HashSet<Integer> s = new HashSet<Integer>();
+        s.add(n);
 
-        return (slow == 1);
+        // Keep replacing n with sum of
+        // squares of digits until we either
+        // reach 1 or we endup in a cycle
+        while (true)
+        {
+
+            // Number is Happy if we reach 1
+            if (n == 1)
+                return true;
+
+            // Replace n with sum of squares
+            // of digits
+            n = sumDigitSquare(n);
+
+            // If n is already visited, a cycle
+            // is formed, means not Happy
+            if ((s.contains(n) && n != (int)s.toArray()[ s.size()-1 ] ))
+                return false;
+
+            // Mark n as visited
+            s.add(n);
+
+        }
+
     }
-    @Override
+    @Override  // Sobre escribo el metodo para devolver los numeros felices
     public String filter(){
         StringBuilder sb = new StringBuilder();
         for(int i=startElement;i<=endElement;i++)
@@ -58,21 +86,81 @@ class ChildTwo extends Parent{
 }
 
 
-public class Pgit olymorphism {
+public class Polymorphism {
     public static void main(String args[] ) throws Exception {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int start = Integer.parseInt(br.readLine());
-        int end = Integer.parseInt(br.readLine());
+      //  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      //  int start = Integer.parseInt(br.readLine());
+     //   int end = Integer.parseInt(br.readLine());
         ChildOne ch1 = new ChildOne();
         ChildTwo ch2 = new ChildTwo();
-        ch1.startElement =  start;
-        ch1.endElement = end;
-        ch2.startElement = start;
-        ch2.endElement = end;
+//        ch1.startElement =  start;
+//        ch1.endElement = end;
+//        ch2.startElement = start;
+//        ch2.endElement = end;
+        ch1.startElement =  1;
+        ch1.endElement = 5555;
+        ch2.startElement = 1;
+        ch2.endElement = 5555;
+        long total = 0;
+        int x;
+        long inicio = System.currentTimeMillis();
         System.out.println(ch1.filter());
         System.out.print(ch2.filter());
+        long tiempo = System.currentTimeMillis() - inicio;
+        total += tiempo;
+        System.out.println("\n");
+        System.out.println("tardo: "+total);
+
     }
 }
 
 
+/*
+*
+*  private int sumarCuadrados(int num){
+
+        System.out.println("Antes del while: "+ num);
+        int res = 0;
+        while(num>0){
+            System.out.println("Entro al while el num: "+ num);
+            int dig = num%10;
+            System.out.println("Dig es num%10 = "+ dig);
+            num = num/10;
+            System.out.println("num/10 es igual a: "+num/10);
+            res = res+(dig*dig);
+            System.out.println("Devuelvo: "+res);
+        }
+        return res;
+    }
+    *
+    *
+    *
+    *
+    *  private Boolean isHappy(int numero){
+        Boolean res = false;
+        int original = numero;
+        boolean bandera = true;
+        while(bandera==true){
+            int sumaCuadrados = sumarCuadrados(numero);
+            if(sumaCuadrados == 1){
+                res = true;
+                bandera = false;
+            }else{
+                if(sumaCuadrados == original){
+                    res = false;
+                    bandera = false;
+                }else{
+                    if(sumaCuadrados<10 && sumaCuadrados!=7){
+                        res = false;
+                        bandera = false;
+                    }else{
+                        numero = sumaCuadrados;
+                     //   System.out.println("SUMACUADRADOS: " + numero);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+* */
